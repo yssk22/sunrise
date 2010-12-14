8d8c3f024b4c7bb415ebfb3fdc909cebd2ce5633
Yohei Sasaki <yssk22@gmail.com>
2010-12-15 00:26:50 +0900

    sunriseHelper fixed.

--- test/test.server.js
+++ test/test.server.js
@@ -25,5 +25,36 @@ module.exports = {
       }, {
          body: "Hello World"
       });
+   },
+
+   "test sunriseHelper" : function(){
+      var app = server.createServer('foo');
+      app.setUp({
+         setUp: function(app){
+            app.get('/', function(req, res, next){
+               assert.isDefined(req.logger);
+               assert.isDefined(res.logger);
+               assert.isDefined(res.bindings);
+               assert.isDefined(res.bindings.page);
+               assert.eql(res.bindings.page.title, "");
+               assert.eql(res.bindings.page.description, "");
+               assert.eql(res.bindings.page.keywords, "");
+               assert.eql(res.bindings.page.javascripts, []);
+               assert.eql(res.bindings.page.stylesheets, []);
+               next();
+            }, function(req, res, next){
+               res.bindings.foo = 'Hello';
+               next();
+            }, function(req, res, next){
+               res.writeHead(200, {'Content-Type': "text/plain"});
+               res.end(res.bindings.foo);
+            });
+         }
+      });
+      assert.response(app, {
+         url: '/', method: "GET"
+      }, {
+         body: "Hello"
+      });
    }
 };

d9bdd2690cd2e5b1c752b97b2b4af4b29df35cba
Yohei Sasaki <yssk22@gmail.com>
2010-12-18 01:54:45 +0900

    uuid and docId proxy for cradle

---
+++ test/test.db.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var helper = require('./helper');
+
+var db = require('sunrise/db');
+
+function setUp(){
+  return db.connect();
+}
+
+module.exports = {
+   "test uuid": function(){
+      var db = setUp();
+      db.uuid(function(err, id){
+         assert.ok(id);
+      });
+   },
+
+   "test docId": function(){
+      var db = setUp();
+      assert.eql(db.docId("bar", "foo"),     "foo:bar");
+      assert.eql(db.docId("foo:bar", "foo"), "foo:bar");
+   }
+}

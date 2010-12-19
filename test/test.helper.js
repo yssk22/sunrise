28f85515745c78eb5c60d7ac16c16d3e4626a153
Yohei Sasaki <yssk22@gmail.com>
2010-12-19 18:13:46 +0900

    rename dynamicDocuemnt to dynamicObject

--- test/test.helper.js
+++ test/test.helper.js
@@ -4,37 +4,37 @@ var assert = require('assert');
 var helper = require('sunrise/helper');
 
 module.exports = {
-   "test dynamicDocument": function(){
+   "test dynamicObject": function(){
       // number, string, boolean, null, undefined
 
-      helper.dynamicDocument(1, function(err, result){
+      helper.dynamicObject(1, function(err, result){
          assert.eql(1, result);
       });
 
-      helper.dynamicDocument("String", function(err, result){
+      helper.dynamicObject("String", function(err, result){
          assert.eql("String", result);
       });
 
-      helper.dynamicDocument(true, function(err, result){
+      helper.dynamicObject(true, function(err, result){
          assert.eql(true, result);
       });
 
-      helper.dynamicDocument(null, function(err, result){
+      helper.dynamicObject(null, function(err, result){
          assert.eql(null, result);
       });
 
-      helper.dynamicDocument(undefined, function(err, result){
+      helper.dynamicObject(undefined, function(err, result){
          assert.eql(undefined, result);
       });
 
       /*
       // Array (Simple)
-      helper.dynamicDocument([1,2,3], function(err, result){
+      helper.dynamicObject([1,2,3], function(err, result){
          assert.eql([1,2,3], result);
       });
 
       // Object (Simple)
-      helper.dynamicDocument({foo: "bar"}, function(err, result){
+      helper.dynamicObject({foo: "bar"}, function(err, result){
          assert.eql({foo: "bar"}, result);
       });
       */
@@ -56,16 +56,16 @@ module.exports = {
       }
 
       // function
-      helper.dynamicDocument(syncFun, ctx, function(err, result){
+      helper.dynamicObject(syncFun, ctx, function(err, result){
          assert.eql("sync", result);
       });
-      helper.dynamicDocument(asyncFun, ctx, function(err, result){
+      helper.dynamicObject(asyncFun, ctx, function(err, result){
          assert.eql("async", result);
       });
 
       // nested objects
       var nested1 = [syncFun, asyncFun, 1];
-      helper.dynamicDocument(nested1, ctx, function(err, result){
+      helper.dynamicObject(nested1, ctx, function(err, result){
          assert.eql("sync", result[0]);
          assert.eql("async", result[1]);
          assert.eql(1, result[2]);
@@ -79,7 +79,7 @@ module.exports = {
          b: asyncFun,
          c: 1
       };
-      helper.dynamicDocument(nested2, ctx, function(err, result){
+      helper.dynamicObject(nested2, ctx, function(err, result){
          assert.eql("sync", result.a);
          assert.eql("async", result.b);
          assert.eql(1, result.c);
@@ -96,7 +96,7 @@ module.exports = {
             a: [syncFun, asyncFun, 1]
          }
       };
-      helper.dynamicDocument(nested3, ctx, function(err, result){
+      helper.dynamicObject(nested3, ctx, function(err, result){
          assert.eql("sync", result.a[0]);
          assert.eql("async", result.a[1]);
          assert.eql(1, result.a[2]);

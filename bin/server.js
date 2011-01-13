d8d2c1b61a44d0428be6a1217a559d76d32ecc0f
Yohei Sasaki <yssk22@gmail.com>
2011-01-13 23:39:14 +0900

    snapshot prototype releasing (not tested yet!)

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var site = require('sunrise/site');
 var config = require('sunrise/config');
 var helper = require('sunrise/helper');
+var error = require('sunrise/error');
 
 var base = helper.abspath(process.argv[2]);
 var appPath = path.join(base, 'app');
@@ -11,19 +12,31 @@ var confDir = path.join(base, 'conf');
 
 // load configuration
 config.load(confDir);
+var s = site.createSite();
 
-var mod = require(appPath);
-var s = site.createSite(base);
-s.setup(mod, function(err){
-   if( err ){
-      throw err;
-   }else{
-      s.logger.info("server initialized from " + appPath);
-      s.listen(config.web.port, function(err){
-         if( err ){
-            throw err;
-         }
-         s.logger.info("listening on " + config.web.port);
-      });
-   }
-});
+s.logger.debug('Constructing sunrise site ...');
+
+s.install(base,
+          { namespace: '/' },
+          function(err, app){
+            if( err ){
+              throw err;
+            }else{
+              // TODO: not found handler
+              // s.all('*', function(req, res, next){
+              //   console.log('fooo');
+              //   error.raise(404);
+              // });
+
+              // finally added error document on 'production' environment.
+              var errordoc = path.join(app.set('views'), 'errors');
+              s.error(error.render(errordoc));
+              s.logger.info("site initialized from " + base);
+              s.listen(config.web.port, function(err){
+                if( err ){
+                  throw err;
+                }
+                s.logger.info("listening on " + config.web.port);
+              });
+            }
+          });

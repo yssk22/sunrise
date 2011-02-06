d25b77da3d06a4a7b9e6c715997fced18259eac4
Yohei Sasaki <yssk22@gmail.com>
2011-02-06 23:09:00 +0900

    prototype release

---
+++ bin/create.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var helper = require('sunrise/helper');
+var exec = require('child_process').exec;
+
+
+var base = helper.abspath(process.argv[2]);
+var template = helper.abspath(__dirname + "/../template");
+
+path.exists(base, function(exists){
+   if( exists ){
+     console.error(base + ' already exists');
+     process.exit(1);
+   }else{
+     console.log('Creating a sunrise site directory');
+     var command = 'cp -r ' + template + ' ' + base;
+     exec(command, function(err, stdout, stderr){
+       if( err ){
+         console.error('Failed to create a sunrise site directory');
+         console.error(stderr);
+         process.exit(err.code);
+       }else{
+         console.log("Your site has been successfully created.");
+         console.log("To start up your site: ");
+         console.log("");
+         console.log("   $ sunrise:server " + base);
+         console.log("");
+       }
+     });
+   }
+});

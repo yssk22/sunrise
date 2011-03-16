068a34ece9b86f92e4e6fc1770a8ae88e3f3069e
Yohei Sasaki <yssk22@gmail.com>
2011-03-16 19:31:07 +0900

    issue #3 helper functions moved to utils.

--- test/test.utils.js
+++ test/test.utils.js
@@ -47,5 +47,121 @@ module.exports = {
     assert.eql(undefined, utils.clone(undefined));
     assert.eql(1, utils.clone(1));
     assert.eql("hoge", utils.clone("hoge"));
+  },
+
+  "test abspath": function(){
+    var root = process.cwd();
+    assert.eql(utils.abspath('/path/to/foo/bar'), '/path/to/foo/bar');
+    assert.eql(utils.abspath('foo'), root + '/foo');
+  },
+
+  "test bindContext": function(){
+    // number, string, boolean, null, undefined
+
+    utils.bindContext(1, function(err, result){
+      assert.eql(1, result);
+    });
+
+    utils.bindContext("String", function(err, result){
+      assert.eql("String", result);
+    });
+
+    utils.bindContext(true, function(err, result){
+      assert.eql(true, result);
+    });
+
+    utils.bindContext(null, function(err, result){
+      assert.eql(null, result);
+    });
+
+    utils.bindContext(undefined, function(err, result){
+      assert.eql(undefined, result);
+    });
+
+    // Array (Simple)
+    utils.bindContext([1,2,3], function(err, result){
+      assert.eql([1,2,3], result);
+    });
+
+    // Object (Simple)
+    utils.bindContext({foo: "bar"}, function(err, result){
+      assert.eql({foo: "bar"}, result);
+    });
+
+    var ctx = {
+      syncResult: "sync",
+      asyncResult: "async"
+    };
+
+    // nested objects with synchronized function evaluation
+    function syncFun(callback){
+      callback(undefined, this.syncResult);
+    }
+
+    function asyncFun(callback){
+      var self = this;
+      return setTimeout(function(){
+        callback(undefined, self.asyncResult);
+      }, 100);
+    }
+
+    // function
+    utils.bindContext(syncFun, ctx, function(err, result){
+      assert.eql("sync", result);
+    });
+    utils.bindContext(asyncFun, ctx, function(err, result){
+      assert.eql("async", result);
+    });
+
+    // nested objects
+    var nested1 = [syncFun, asyncFun, 1];
+    utils.bindContext(nested1, ctx, function(err, result){
+      assert.eql("sync", result[0]);
+      assert.eql("async", result[1]);
+      assert.eql(1, result[2]);
+      // confirm no changes are in input object;
+      assert.eql(syncFun, nested1[0]);
+      assert.eql(asyncFun, nested1[1]);
+      assert.eql(1, nested1[2]);
+    });
+    var nested2 = {
+          a: syncFun,
+      b: asyncFun,
+      c: 1
+    };
+    utils.bindContext(nested2, ctx, function(err, result){
+      assert.eql("sync", result.a);
+      assert.eql("async", result.b);
+      assert.eql(1, result.c);
+
+      // confirm no changes are in input object;
+      assert.eql(syncFun, nested2.a);
+      assert.eql(asyncFun, nested2.b);
+      assert.eql(1, nested2.c);
+    });
+
+    var nested3 = {
+      a: [syncFun, asyncFun, 1],
+      b: {
+        a: [syncFun, asyncFun, 1]
+      }
+    };
+    utils.bindContext(nested3, ctx, function(err, result){
+      assert.eql("sync", result.a[0]);
+      assert.eql("async", result.a[1]);
+      assert.eql(1, result.a[2]);
+      assert.eql("sync", result.b.a[0]);
+      assert.eql("async", result.b.a[1]);
+      assert.eql(1, result.b.a[2]);
+
+      // confirm no changes are in input object;
+      assert.eql(syncFun, nested3.a[0]);
+      assert.eql(asyncFun, nested3.a[1]);
+      assert.eql(1, nested3.a[2]);
+      assert.eql(syncFun, nested3.b.a[0]);
+      assert.eql(asyncFun, nested3.b.a[1]);
+      assert.eql(1, nested3.b.a[2]);
+    });
+
   }
-}
+};
